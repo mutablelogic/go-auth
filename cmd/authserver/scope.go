8b635db05009481c6101c84c13d8111c2c0a4039
David Thorpe <djt@mutablelogic.com>
2026-03-20 17:48:17 +0100

    More scope work

---
+++ cmd/authserver/scope.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	// Packages
+	schema "github.com/djthorpe/go-auth/schema"
+	server "github.com/mutablelogic/go-server"
+)
+
+type ScopeCommands struct {
+	Scopes ListScopesCommand `cmd:"" name:"scopes" help:"Get Scopes." group:"USERS & GROUPS"`
+}
+
+type ListScopesCommand struct {
+	schema.ScopeListRequest
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// COMMANDS
+
+func (cmd *ListScopesCommand) Run(ctx server.Cmd) error {
+	client, _, err := clientFor(ctx)
+	if err != nil {
+		return err
+	}
+	scopes, err := client.ListScopes(ctx.Context(), cmd.ScopeListRequest)
+	if err != nil {
+		return err
+	}
+	fmt.Println(scopes)
+	return nil
+}
